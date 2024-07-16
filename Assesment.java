1a4d3d128e702bd693af08bf85b2e6bb4ee76e06
duyhv203 <hoang20duy03@gmail.com>
2024-07-17 01:04:15 +0700

    Create Assesment.java

---
+++ Assesment.java
@@ -0,0 +1,45 @@
+package model;
+
+/**
+ *
+ * @author X1 Nano
+ */
+public class Assessment {
+    private int id;
+    private String name;
+    private float weight;
+    private Subject subject;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public float getWeight() {
+        return weight;
+    }
+
+    public void setWeight(float weight) {
+        this.weight = weight;
+    }
+
+    public Subject getSubject() {
+        return subject;
+    }
+
+    public void setSubject(Subject subject) {
+        this.subject = subject;
+    }
+    
+}
